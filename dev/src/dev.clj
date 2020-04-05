5b1988f87c1caa66a912ff5c4b0ecc8dc0d51741
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-04-06 00:20:49 +0900

    Expound & Orchestra

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -4,4 +4,16 @@
             [clojure.spec.alpha :as s]
             [clojure.test :as test]
             [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.spec.test.alpha :as stest]))
+            [clojure.spec.test.alpha :as stest]
+            [expound.alpha :as expound]
+            [orchestra.spec.test :as orchestra.stest]))
+
+(comment
+
+  ;; Enable Expound custom explain printer
+  (set! s/*explain-out* (expound/custom-printer {:theme :figwheel-theme}))
+
+  ;; Enable default explain printer
+  (set! s/*explain-out* s/explain-printer)
+
+  )
